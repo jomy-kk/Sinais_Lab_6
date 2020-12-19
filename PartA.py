26adb2062239474ebae950864c1f8be8f0e02253
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2020-12-19 19:44:32 +0000

    PartB fixed

--- PartA.py
+++ PartA.py
@@ -1,13 +1,10 @@
-from scipy import signal
+from scipy.io import savemat
 import matplotlib.pyplot as plt
 import numpy as np
-import random
-import scipy.stats
 from math import log10
 
-k=0.495 #
-sampling_rate = 500 #Hz
-
+k = 0.495
+sampling_rate = 200 #Hz
 
 def signaltonoise(a, axis=0, ddof=0):
     a = np.asanyarray(a)
@@ -19,11 +16,14 @@ def decibeis (a):
     db=20*log10(a)
     return db
 
-
 t = np.linspace(0, 2, sampling_rate, endpoint=False)
+sig = np.sin(100 * t) + k
+snr = decibeis(signaltonoise(sig))
+savemat('sin'+str(int(snr))+'.mat', {"signal": sig, "snr": snr})
+
 plt.figure()
-sig = np.sin(100 * t)+ k*random.random()
-plt.plot(t,sig)
-print(signaltonoise(sig))
-print(decibeis(signaltonoise(sig)))
+plt.plot(t, sig)
+plt.title('SNR = ' + str(int(snr)) + ' dB dB (k=' + str(k) + ')')
+plt.xlabel('Time (ms)')
+plt.ylabel('Voltage (V)')
 plt.show()
