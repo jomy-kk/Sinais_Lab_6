b0569b4a8e9aedf59be0466203b2e30a2fec6ede
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2020-12-20 16:00:19 +0000

    Refactored a_3

--- A/a_3.py
+++ A/a_3.py
@@ -1,33 +1,47 @@
 from scipy.io import loadmat
 import os
-os.chdir("D:/faculdade/5º ano/Projeto de Sensores, Sinais e Instrumentação/Labs/Lab 6") #Tive que fazer isto para conseguir usar os dados das imagens
-import scipy as sp
-from scipy import signal
+#os.chdir("D:/faculdade/5º ano/Projeto de Sensores, Sinais e Instrumentação/Labs/Lab 6") #Tive que fazer isto para conseguir usar os dados das imagens
+from scipy.ndimage import median_filter as mf
 import matplotlib.pyplot as plt
 import numpy as np
 
-sampling_rate = 200 #Hz
-samples = 3 #number of samples to use in filter
+def median_filter(signals, samples, sampling_frequency, sampling_time, verbose):
+    '''
+    Applied the median filter to one or more signals.
+    :param signals: One or list of 2+ Ndarrays of signals
+    :param samples: number of samples to use in filter
+    :param sampling_frequency: sampling frequency of signal
+    :param sampling_time: duration of signal acquisition
 
-t = np.linspace(0, 2, sampling_rate, endpoint=False)
-sample_3 = loadmat('sin-3.mat')
-sample0 = loadmat('sin0.mat')
-sample20 = loadmat('sin20.mat')
+    (Optionals)
+    :param verbose: if True, prints the variances
 
-signal_3= sample_3['signal']
-signal0 = sample0['signal']
-signal20 = sample20['signal']
+    :return: Nothing
+    '''
+    #sampling_rate = 200 #Hz
+    #samples = 3 #
 
-y_3=sp.ndimage.median_filter(signal_3,samples)
-y0=sp.ndimage.median_filter(signal0,samples)
-y20=sp.ndimage.median_filter(signal20,samples)
+    t = np.linspace(0, sampling_time, sampling_frequency, endpoint=False)
 
-fig, (ax1, ax2, ax3) = plt.subplots(3)
-fig.suptitle('Isto está a fazer algo')
-ax1.plot(t, signal_3[0])
-ax1.plot(t, y_3[0])
-ax2.plot(t, signal0[0])
-ax2.plot(t, y0[0])
-ax3.plot(t, signal20[0])
-ax3.plot(t, y20[0])
-plt.show()
+    Y = []
+    for s in signals:
+        y = mf(s, samples)
+        Y.append(y)
+
+    fig, AX = plt.subplots(len(signals))
+    fig.suptitle('Isto está a fazer algo')
+    for i in range(len(signals)):
+        AX[i].plot(t, signals[i])
+        AX[i].plot(t, Y[i])
+    fig.savefig('results_a3/Median Filters (S=' + str(samples) + ').png', bbox_inches='tight')
+    if verbose:
+        plt.show()
+
+    return
+
+sA = loadmat('sin-3.mat')['signal']
+sB = loadmat('sin0.mat')['signal']
+sC = loadmat('sin20.mat')['signal']
+
+signals = [np.reshape(sA, sA.size), np.reshape(sB, sB.size), np.reshape(sC, sC.size)]
+median_filter(signals, 3, 200, 2, verbose=True)
